13f4a02ac3ef5e8bc4de0673d66224787605f654
Paula Druetta <druetta.paula@gmail.com>
2022-10-06 23:01:19 -0300

    First frontend commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './core/components/button/button.component';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent
+  ],
+  imports: [
+    CoreModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
