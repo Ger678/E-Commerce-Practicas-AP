beb7454419c1ccb36be8b2b6961ada1d9675e9fa
Ger678 <95255946+Ger678@users.noreply.github.com>
2022-10-28 13:08:50 -0300

    Pagina Shopping Cart y Modal de producto

--- frontend/src/app/modules/shopping-cart/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/modules/shopping-cart/pages/cart-page/cart-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../service/rest.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
+  public cartProduct: any = [];
+  public id: number = 1;
+  public amount: number = 1;
+  public price: number = this.cartProduct.price;
+  public images: any = [];
 
-  constructor() { }
+  constructor(private restService: RestService) { }
 
   ngOnInit(): void {
+    this.getData(this.id)
+    this.imagesOfProducts(this.id)
   }
 
+  public getData(id: number){
+    this.restService.get(`https://dummyjson.com/products/${id}`).subscribe(data => {
+      this.cartProduct = data
+      console.log(data)
+    })
+  }
+
+  public addAmount(){
+    this.amount += 1;
+    this.price = this.cartProduct.price * this.amount;
+    console.log(this.price)
+  }
+
+  public removeAmount() {
+    this.amount -= 1;
+    this.price -= this.cartProduct.price;
+    console.log(this.price)
+  }
+
+    //Request for product images
+    public imagesOfProducts(id: number){
+      this.restService
+      .get(`https://dummyjson.com/products/${id}?select=images`)
+      .subscribe((data) => {
+        this.images = Object.values(data);
+        console.log(this.images)
+      })
+    }
 }
--- frontend/src/app/modules/shopping-cart/shopping-cart.module.ts
+++ frontend/src/app/modules/shopping-cart/shopping-cart.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -9,7 +10,8 @@ import { CartPageComponent } from './pages/cart-page/cart-page.component';
     CartPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
 export class ShoppingCartModule { }
