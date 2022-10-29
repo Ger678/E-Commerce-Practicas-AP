2bbd4bb4e83f3703fe9fce718a10cf2a9eeff6e7
Paula Druetta <druetta.paula@gmail.com>
2022-10-29 17:22:11 -0300

    ADD fontawesone

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './modules/home/home.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faArrowDown as faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -13,9 +16,14 @@ import { HomeModule } from './modules/home/home.module';
     CoreModule,
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor (library: FaIconLibrary) {
+    library.addIcons(faArrowDown);
+  }
+}
