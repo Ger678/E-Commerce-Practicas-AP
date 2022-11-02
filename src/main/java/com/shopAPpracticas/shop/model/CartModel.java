368f5abd9441c23667a375df7b3fc4ef62aad3e2
Pitux <padanguina@gmail.com>
2022-11-02 11:43:41 -0300

    Created Cart Model

--- src/main/java/com/shopAPpracticas/shop/model/CartModel.java
+++ src/main/java/com/shopAPpracticas/shop/model/CartModel.java
@@ -1,4 +1,15 @@
 package com.shopAPpracticas.shop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+ @Table(name = "cart")
 public class CartModel {
+
+    @Id
+    private int cart_id;
+    private int customer_id;
+
 }
